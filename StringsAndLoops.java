5d32c602ba5f74491ac84e1fd97b324ba871861a
dmohle <10038913+dmohle@users.noreply.github.com>
2023-08-22 11:52:05 -0700

    Add files via upload

--- StringsAndLoops.java
+++ StringsAndLoops.java
@@ -15,5 +15,11 @@ public class StringsAndLoops {
             System.out.println("myLoopControlVariable is..." + myLoopControlVariable);
             myLoopControlVariable++;
         }
+
+        String[] myStrArray = {"this", "that", "the other"};
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("myStrArray[" + i + "] = " + myStrArray[i]);
+        }
     }
 }
