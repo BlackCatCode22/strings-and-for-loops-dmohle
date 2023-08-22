fd09489bb53c9c9262b4854eeb83124377002701
dmohle <10038913+dmohle@users.noreply.github.com>
2023-08-22 11:41:52 -0700

    Add files via upload

---
+++ StringsAndLoops.java
@@ -0,0 +1,19 @@
+
+public class StringsAndLoops {
+    public static void main(String[] args) {
+
+        System.out.printf("\nWelcome to Strings and Loops!\n");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("i = " + i);
+        }
+
+        int myLoopControlVariable = 0;
+
+        while (myLoopControlVariable <= 12){
+            System.out.println("myLoopControlVariable is..." + myLoopControlVariable);
+            myLoopControlVariable++;
+        }
+    }
+}
